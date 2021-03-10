828335b5275c3f4ec0961423f792c166b93d89be
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-03-10 16:50:15 -0700

    error message ignores on index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+// @ts-ignore
 import AboutPage from '../pages/AboutPage.vue'
+// @ts-ignore
 import CarDetailsPage from '../pages/CarDetailsPage.vue'
+// @ts-ignore
 import CarsPage from '../pages/CarsPage.vue'
+// @ts-ignore
 import HomePage from '../pages/HomePage.vue'
+// @ts-ignore
 import HouseDetailsPage from '../pages/HouseDetailsPage.vue'
+// @ts-ignore
 import HousesPage from '../pages/HousesPage.vue'
+// @ts-ignore
 import JobDetailsPage from '../pages/JobDetailsPage.vue'
+// @ts-ignore
 import JobsPage from '../pages/JobsPage.vue'
 
 const routes = [
