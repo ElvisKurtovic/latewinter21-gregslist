9c66e71d6ff10175aa4dd7f3d1696166169c6711
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-03-10 14:14:57 -0700

    added houses

---
+++ src/pages/HouseDetailsPage.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="house-details">
+    <h1>WElcome to the details page</h1>
+    {{ state.house }}
+    <button type="button" class="btn btn-outline-danger" @click="deleteHouse">
+      Delete House
+    </button>
+
+    <form class="form-inline" onsubmit="app.housesController.createHouse(event)">
+      <div class="form-group">
+        <input
+          type="text"
+          name="make"
+          id="make"
+          class="form-control"
+          placeholder="Make"
+          aria-describedby="helpId"
+          v-model="state.house.make"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          name="model"
+          id="model"
+          class="form-control"
+          placeholder="Model"
+          aria-describedby="helpId"
+          v-model="state.house.model"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="number"
+          name="year"
+          id="year"
+          class="form-control"
+          placeholder="Year"
+          aria-describedby="helpId"
+          v-model="state.house.year"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="number"
+          name="price"
+          id="price"
+          class="form-control"
+          placeholder="Price"
+          aria-describedby="helpId"
+          v-model="state.house.price"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          name="description"
+          id="description"
+          class="form-control"
+          placeholder="Description"
+          aria-describedby="helpId"
+          v-model="state.house.description"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          name="imgUrl"
+          id="imgUrl"
+          class="form-control"
+          placeholder="ImgUrl"
+          aria-describedby="helpId"
+          v-model="state.house.imgUrl"
+        />
+      </div>
+      <button class="btn btn-info" type="submit">Create</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { onMounted, reactive, computed } from 'vue'
+import { onBeforeRouteLeave, useRoute, useRouter } from 'vue-router'
+import { housesService } from '../services/HousesService'
+import { AppState } from '../Appstate'
+
+export default {
+  name: 'HouseDetails',
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const state = reactive({
+      house: computed(() => AppState.activeHouse)
+    })
+
+    onMounted(() => {
+      housesService.getHouse(route.params.id)
+    })
+
+    onBeforeRouteLeave((to, from, next) => {
+      if (window.confirm('You sure bro?')) {
+        AppState.activeHouse = {}
+        next()
+      }
+    })
+
+    return {
+      route,
+      state,
+      async deleteHouse() {
+        await housesService.deleteHouse(state.house._id)
+        router.push({ name: 'Houses' })
+      }
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import AboutPage from '../pages/AboutPage.vue'
 import CarDetailsPage from '../pages/CarDetailsPage.vue'
 import CarsPage from '../pages/CarsPage.vue'
 import HomePage from '../pages/HomePage.vue'
+import HouseDetailsPage from '../pages/HouseDetailsPage.vue'
+import HousesPage from '../pages/HousesPage.vue'
 
 const routes = [
   {
@@ -24,6 +26,16 @@ const routes = [
     path: '/cars/:id',
     name: 'CarDetails',
     component: CarDetailsPage
+  },
+  {
+    path: '/houses',
+    name: 'Houses',
+    component: HousesPage
+  },
+  {
+    path: '/houses/:id',
+    name: 'HouseDetails',
+    component: HouseDetailsPage
   }
 ]
 
